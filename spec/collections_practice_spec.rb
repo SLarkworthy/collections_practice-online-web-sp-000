1102be0fd7d239b84b115124b23306782a687f60
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-30 18:13:09 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -32,8 +32,8 @@ describe 'collections practice' do
 
 # Question 4 advanced
 describe '#swap_elements_from_to' do
-    it 'swap the second and third elements of an array' do
-      expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
+    it 'swap elements in an array at a start and destination index' do
+      expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
     end
   end
 
